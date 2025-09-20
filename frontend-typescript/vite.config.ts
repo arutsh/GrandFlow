351e4318f09922624c3a2e94fba4a7c36c02469c
Norair Arutshyan <n.arutshyan@gmail.com>
2025-09-20 13:40:06 +0100

    simple testing with github action for frontend (#21)
    * simple testing with github action for frontend
    
    * installing missing package

--- frontend-typescript/vite.config.ts
+++ frontend-typescript/vite.config.ts
@@ -1,6 +1,8 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+
 
 // use process.cwd() to avoid import.meta.url complexity
 export default defineConfig({
@@ -14,4 +16,10 @@ export default defineConfig({
     port: 3000, // 👈 expose frontend at http://localhost:3000
     host: true, // needed for Docker
   },
-});
+  test: {
+    globals: true, // so you can use describe/it/expect without imports
+    environment: "jsdom", // so React can render
+    setupFiles: ["./src/setupTests.ts"], // optional, for global setup
+  },
+
+})
